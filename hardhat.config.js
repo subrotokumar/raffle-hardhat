8def597f8d4cdf1b7169706e637eef2d446de4f5
Subroto Kumar <subrotokumar@outlook.in>
2022-08-01 16:05:12 +0530

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,67 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://subrotokumar.com"
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || "https://subrotokumar.com"
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        rinkeby: {
+            chainId: 4,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+        mumbai: {
+            chainId: 80001,
+            url: MUMBAI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.8.4",
+            },
+        ],
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+            mumbai: POLYGONSCAN_API_KEY,
+        },
+    },
+    // gasReporter: {
+    //     enabled: REPORT_GAS,
+    //     currency: "USD",
+    //     outputFile: "gas-report.txt",
+    //     noColors: true,
+    //     coinmarketcap: COINMARKETCAP_API_KEY,
+    // },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+}
---
+++ helper-hardhat-config.js
@@ -0,0 +1,46 @@
+const { ethers } = require("ethers")
+
+const networkConfig = {
+    default: {
+        name: "hardhat",
+        keepersUpdateInterval: "30",
+    },
+    31337: {
+        name: "localhost",
+        subscriptionId: "588",
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+    },
+    4: {
+        name: "rinkeby",
+        subscriptionId: "6926",
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+    },
+    80001: {
+        name: "mumbai",
+        vrfCoordinatorV2: "0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed",
+        raffleEntranceFee: "100000000000000000",
+        gasLane: "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f",
+        callbackGasLimit: "500000",
+        keepersUpdateInterval: "60",
+    },
+    1: {
+        name: "mainnet",
+        keepersUpdateInterval: "30",
+    },
+}
+
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+}
